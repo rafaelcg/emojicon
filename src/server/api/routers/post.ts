de61d7fc4e2df27d99f2411bbbc478b2b6752fcb
Rafael Cammarano Guglielmi <rafaelcg@gmail.com>
2024-04-11 00:22:06 +0100

    Next image tag, styles

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,18 @@
+import { User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    profileImageUrl: user.imageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
   getLatest: publicProcedure.query(({ ctx }) => {
@@ -8,7 +20,33 @@ export const postRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     });
   }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Author not found",
+        });
+      }
+      
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
