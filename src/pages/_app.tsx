f85df2f9ec62097156e59b4c9b3aeaa3b8dae9ad
Rafael Cammarano Guglielmi <rafaelcg@gmail.com>
2024-04-10 23:10:56 +0100

    added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,19 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
-      <Component {...pageProps} />
-    </main>
+    <ClerkProvider>
+      <main className={`font-sans ${inter.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    </ClerkProvider>
   );
 };
 
